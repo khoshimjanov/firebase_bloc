d29452ed0f396178b9b2ebd04dd8975c2e6d269b
Dovudhon <dovudxon.2006.uz@gmail.com>
2024-01-19 16:19:20 +0500

    second commit

---
+++ productive/lib/features/tasks/data/data_source/remote.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:productive/core/exception/exception.dart';
+
+import '../models/task.dart';
+
+abstract class TaskRemoteDataSource {
+  Future<List<TaskModel>> getTasks();
+
+  factory TaskRemoteDataSource() => _TaskRemoteDataSourceImpl();
+}
+
+class _TaskRemoteDataSourceImpl implements TaskRemoteDataSource {
+  @override
+  Future<List<TaskModel>> getTasks() async {
+    try {
+      final collection =
+          await FirebaseFirestore.instance.collection('tasks').get();
+
+      final tasks = collection.docs
+          .map((item) => TaskModel.fromJson(item.data(), item.id))
+          .toList();
+
+      return tasks;
+    } catch (error) {
+      print(error);
+      throw ServerException(message: 'Xatolik yuz berdi!', code: 500);
+    }
+  }
+}
